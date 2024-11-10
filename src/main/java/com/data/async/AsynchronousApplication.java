c4bec7f3ccf4fad1f127bd42b0a5fe5a5611ab73
sulakshana.singh <r.sulakshana@yahoo.com>
2024-11-09 22:48:32 -0600

    format code

--- src/main/java/com/data/async/AsynchronousApplication.java
+++ src/main/java/com/data/async/AsynchronousApplication.java
@@ -28,18 +28,13 @@ public class AsynchronousApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//STEP1: Load employee dataset
 		String csvPath ="C:\\Users\\project\\asynchronous\\src\\main\\resources\\employee.csv";
-
+		//STEP1: Load employee dataset
 		List<Employee> employeeList = new CsvToBeanBuilder<Employee>(new FileReader(csvPath))
-				.withType(Employee.class)
-				.build()
-				.parse();
-
+				.withType(Employee.class).build().parse();
 		//STEP2: Group employees by department
 		Map<String, List<Employee>> departmentEmployees = employeeList.stream()
 				.collect(Collectors.groupingBy(e -> e.getDept()));
-
 		//STEP3: Configure CyclicBarrier to ensure synchronization between threads
 		CyclicBarrier barrier = new CyclicBarrier(departmentEmployees.size(), () -> {
 			System.out.println("All departments processed. Average salaries:");
@@ -48,7 +43,6 @@ public class AsynchronousApplication implements CommandLineRunner {
 		});
 		//STEP4: Executor for asynchronous processing
 		ExecutorService executor = Executors.newCachedThreadPool();
-
 		//STEP5: Process each department asynchronously
 		List<CompletableFuture<Void>> futures = departmentEmployees.entrySet().stream()
 				.map(entry -> CompletableFuture.runAsync(() -> {
@@ -62,12 +56,9 @@ public class AsynchronousApplication implements CommandLineRunner {
 					}
 				}, executor))
 				.collect(Collectors.toList());
-
 		//STEP6: Wait for all tasks to complete
 		CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-
 		executor.shutdown();
-
 	}
 	private static void calculateAndStoreAverage(String department, List<Employee> employees) {
 		double average = employees.stream()
